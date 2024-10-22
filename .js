4da0bbb5f727798859c07957133a86ef8ee6b25c
kigi160 <gajvoronskaakristina47@gmail.com>
2024-10-22 20:11:01 +0300

    да

---
+++ .js
@@ -0,0 +1,9 @@
+window.onload = function()
+{
+    const name = "Гайворонская Кристина";
+    const element = document.querySelector("#name");
+    if (element)
+    {
+        element.textContent = name;
+    }
+}
